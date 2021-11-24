cbad738c4626e504c41fdcfde2497626e27c0af4
lk101 <601441669@qq.com>
2021-11-24 13:15:37 +0800

    Create JSONable.java

---
+++ org/javascript/JSONable.java
@@ -0,0 +1,12 @@
+package org.javascript;
+
+/**
+ * Define the JSONable interface.
+ */
+public interface JSONable {
+	/**
+	 * Output this object as a JSON structure.
+	 * @return
+	 */
+	Object toJSON();
+}
